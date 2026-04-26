ffbc8999a1ca8458b69ead77dcbcd8f4df6db07a
Mark Shovman <mark.shovman@gmail.com>
2026-04-26 19:25:10 +0300

    Add vergence analysis code and figures 12-13

--- Code/analyse_vergence.py
+++ Code/analyse_vergence.py
@@ -1,3 +1,7 @@
+"""
+Figures 12 and 13 in the paper
+"""
+
 import numpy as np
 import pandas as pd
 
@@ -6,8 +10,10 @@ import seaborn as sns
 
 import os
 
+from Code.utils import calculate_vergence_point, unit_to_az_el, collate_series
+
 # %%
-df_all = None
+df_list = []
 for fname in os.listdir("Data/3_vergence"):
     print(f"  {fname}")
     pid, _, session = fname.split(".")[0].split("_")
@@ -15,5 +21,109 @@ for fname in os.listdir("Data/3_vergence"):
     df["experiment"] = fname.split(".")[0]
     df["participant"] = pid
     df["session"] = session
-    df_all = pd.concat((df_all,df)) if df_all is not None else df
+    df_list.append(df)
+
+df_all = pd.concat(df_list, ignore_index=True)
+
+
+#%%
+
+vp, div = calculate_vergence_point(df_all[[f'ic{x}_left' for x in 'XYZ']].values,
+                                   -df_all[[f'ig{x}_left' for x in 'XYZ']].values,
+                                   df_all[[f'ic{x}_right' for x in 'XYZ']].values,
+                                   -df_all[[f'ig{x}_right' for x in 'XYZ']].values)
+
+df_all[['vergence_point_x', 'vergence_point_y', 'vergence_point_z']] = vp
+df_all["divergence"] = div
+
+df_all['x_left'], df_all['y_left'] = unit_to_az_el(-df_all[[f'ig{x}_left' for x in 'XYZ']].values, convention='fick')
+df_all['x_right'], df_all['y_right'] = unit_to_az_el(-df_all[[f'ig{x}_right' for x in 'XYZ']].values, convention='fick')
+
+#%%
+experiment = "p010_vergence_2"
+
+df = df_all[df_all["experiment"] == experiment].copy().set_index("timestamp_sec", drop=True)
+
+fixations = collate_series(df.reference_fixation_id)
+#%% Figure 12
+fig, axs = plt.subplots(3, 1, sharex=True, num=experiment)
+
+for eye in ["left", "right"]:
+    c = "b" if eye == "left" else "r"
+    df[[f'x_{eye}', f'y_{eye}']].plot(ax=axs[:2], subplots=True, color=c, legend=False)
+
+for t_start, t_end, s in collate_series(df.stimulus_order_from_viewers):
+    if pd.isna(s):
+        continue
+    for ax in axs:
+        ax.axvline(t_start, linestyle=":", color="k")
+        ax.axvline(t_end, linestyle=":", color="k")
+
+(-df["vergence_point_z"]).plot(ax=axs[-1], c="k")
+
+for t_start, t_end, s in collate_series(df.reference_fixation_id):
+    if pd.isna(s):
+        continue
+    for ax in axs:
+        ax.axvspan(t_start, t_end, color="cyan")
+        # ax.axvline(t_start, linestyle=":", color="k", alpha=0.5)
+        # ax.axvline(t_end, linestyle=":", color="k", alpha=0.5)
+    y = (-df.loc[t_start:t_end, "vergence_point_z"]).mean()
+    axs[2].plot((t_start, t_end), (y, y), color="k")
+    axs[2].text(
+        t_start, y, f"[{s}]\n{y:.2f}", ha="right", va="center", weight="bold"
+    )
+
+axs[0].set_ylabel("Azimuth (deg)")
+axs[1].set_ylabel("Elevation (deg)")
+axs[2].set_ylabel("VP distance from head (mm)")
+
+axs[-1].set_xlabel("Time (sec)")
+
+for ax in axs:
+    ax.grid()
+
+#%% Figure 13
+fix = pd.DataFrame(-(df_all[~df_all.reference_fixation_id.isna()].groupby(["stimulus_order_from_viewers", "experiment"]).vergence_point_z.mean()))
+
+plt.figure()
+sns.boxplot(
+    y="vergence_point_z",
+    x="stimulus_order_from_viewers",
+    color="lightgreen",
+    legend=False,
+    data=fix,
+    log_scale=False,
+    fliersize=0,
+    capwidths=0,
+)
+sns.swarmplot(
+    y="vergence_point_z",
+    x="stimulus_order_from_viewers",
+    color="green",
+    legend=False,
+    data=fix,
+    size=2,
+)
+m = fix.groupby("stimulus_order_from_viewers")["vergence_point_z"].mean()
+sd = fix.groupby("stimulus_order_from_viewers")["vergence_point_z"].std()
+txt = {t: f"{m[t]:.2f}±{sd[t]:.2f}" for t in m.index}
+for t in m.index:
+    plt.text(
+        t - 1,
+        m[t] + 0.1,
+        f"{m[t]:.2f}±{sd[t]:.2f}",
+        ha="center",
+        va="bottom",
+        fontsize=8,
+    )
+plt.plot(m.index - 1, m, "k^")
+
+plt.grid(axis="y")
+plt.xlabel("")
+plt.ylabel("Vergence Point - Distance to Marker (mm)")
+plt.xlabel("Depth Order from Participant")
+plt.ylim(-200, 800)
+# plt.title(title)
+plt.tight_layout()
 
--- Code/utils.py
+++ Code/utils.py
@@ -61,12 +61,17 @@ def collate_series(s, min_duration=0):
 
     """
 
+    def values_equal(v1, v2):
+        if pd.isna(v1) and pd.isna(v2):
+            return True
+        return v1 == v2
+
     intervals = []
     t_start = s.index[0]
     current_value = s.iloc[0]
 
     for i in range(1, len(s)):
-        if s.iloc[i] != current_value:
+        if not values_equal(s.iloc[i], current_value):
             t_end = (s.index[i - 1] + s.index[i]) / 2
             if t_end - t_start >= min_duration:
                 intervals.append([t_start, t_end, current_value])
@@ -74,7 +79,7 @@ def collate_series(s, min_duration=0):
                 current_value = s.iloc[i]
             else:
                 if intervals:
-                    if intervals[-1][2] == s.iloc[i]:
+                    if values_equal(intervals[-1][2], s.iloc[i]):
                         t_start = intervals[-1][0]
                         current_value = intervals[-1][2]
                         del intervals[-1]
@@ -91,3 +96,73 @@ def collate_series(s, min_duration=0):
         intervals[-1][1] = s.index[-1]
 
     return intervals
+
+
+def calculate_vergence_point(o1, d1, o2, d2, epsilon=1e-9, bias=0.5):
+    """
+    Vectorized version of calculate_vergence_point.
+    Inputs are (N, 3) arrays.
+    """
+    w0 = o1 - o2  # (N, 3)
+
+    a = np.sum(d1 * d1, axis=1)  # (N,)
+    b = np.sum(d1 * d2, axis=1)  # (N,)
+    c = np.sum(d2 * d2, axis=1)  # (N,)
+    d = np.sum(d1 * w0, axis=1)  # (N,)
+    e = np.sum(d2 * w0, axis=1)  # (N,)
+
+    denominator = a * c - b * b  # (N,)
+
+    # Initialize results
+    vergence_point = np.full((len(o1), 3), np.nan)
+    divergence = np.full(len(o1), np.inf)
+
+    # Mask for non-parallel rays
+    valid_mask = np.abs(denominator) >= epsilon
+
+    t1 = np.full(len(o1), np.nan)
+    t2 = np.full(len(o1), np.nan)
+
+    t1[valid_mask] = (b[valid_mask] * e[valid_mask] - c[valid_mask] * d[valid_mask]) / denominator[valid_mask]
+    t2[valid_mask] = (a[valid_mask] * e[valid_mask] - b[valid_mask] * d[valid_mask]) / denominator[valid_mask]
+
+    # Mask for rays pointing towards each other (t1 > 0 and t2 > 0)
+    convergence_mask = valid_mask & (t1 >= 0) & (t2 >= 0)
+
+    p1_closest = o1[convergence_mask] + t1[convergence_mask, np.newaxis] * d1[convergence_mask]
+    p2_closest = o2[convergence_mask] + t2[convergence_mask, np.newaxis] * d2[convergence_mask]
+
+    vergence_point[convergence_mask] = p1_closest * bias + p2_closest * (1.0 - bias)
+    divergence[convergence_mask] = np.linalg.norm(p1_closest - p2_closest, axis=1)
+
+    return vergence_point, divergence
+
+
+def unit_to_az_el(v, convention='fick'):
+    """
+    Convert 3D cartesian gaze vectors to angular coordinates (azimuth/elevation).
+
+    Parameters
+    ----------
+    v : array-like, shape (n, 3)
+        Gaze vectors with columns [x, y, z].
+    convention : str, default='fick'
+        'fick' or 'spherical'
+
+    Returns
+    -------
+    azimuth, elevation : tuple of arrays in degrees
+    """
+    x, y, z = v[:, 0], v[:, 1], v[:, 2]
+
+    if convention == 'fick':
+        az = np.degrees(np.arctan2(x, z))
+        el = np.degrees(np.arctan2(y, z))
+    elif convention == 'spherical':
+        hyp = np.sqrt(x**2 + z**2)
+        az = np.degrees(np.arctan2(x, z))
+        el = np.degrees(np.arctan2(y, hyp))
+    else:
+        raise ValueError(f"Unknown convention '{convention}'. Use either 'fick' (default) or 'spherical'.")
+
+    return az, el
